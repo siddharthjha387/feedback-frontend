49f57dd240e72ad6850dade2646adc2730ba5e7e
siddharthjha387 <siddharthjha3901@gmail.com>
2022-12-23 22:18:53 +0530

    changed backend

--- src/Api_Url.js
+++ src/Api_Url.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://backend-restaurant-feedback-production.up.railway.app";
+// export const API_URL = "https://backend-restaurant-feedback-production.up.railway.app";
 // export const API_URL = "http://localhost:8800";
+export const API_URL = "https://quickfeed-backend.onrender.com";
