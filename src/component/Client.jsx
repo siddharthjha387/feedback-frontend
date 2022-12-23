d41e5ebdf5a94c8c86f10e89b85d5430db7b13ff
siddharthjha387 <siddharthjha3901@gmail.com>
2022-12-23 09:23:45 +0530

    responsive client done

--- src/component/Client.jsx
+++ src/component/Client.jsx
@@ -8,7 +8,7 @@ import './client.css'
 import Navbar from './Navbar';
 import { API_URL } from "../Api_Url";
 axios.defaults.withCredentials = true;
-export default function Client({ myStorage,currentUser,setCurrentUser }) {
+export default function Client({ myStorage, currentUser, setCurrentUser }) {
 
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
@@ -29,36 +29,33 @@ export default function Client({ myStorage,currentUser,setCurrentUser }) {
   if (!currentUser)
   return <Navigate replace to="/" />;
   else
-    return (
-      <>
-      <Navbar myStorage={myStorage} setCurrentUser={setCurrentUser} currentUser={currentUser}/>
-    
+  return (
+    <>
+      <Navbar myStorage={myStorage} setCurrentUser={setCurrentUser} currentUser={currentUser} />
+      <div className="container">
+
         <div className="client-page">
           <div className="greet">
-            Hello {currentUser.fullName}! 😊
+            Hello currentUser.fullName! 😊
           </div>
 
         </div>
-          <h2 className="feedback-title">
-            Customer feedbacks for {currentUser.hotelFullName}:
-          </h2>
+        <h2 className="feedback-title">
+          Customer feedbacks for currentUser.hotelFullName:
+        </h2>
         <div className='outer-container-feedback'>
-        {reviews.map((review) => (
-          <div className="inner-container">
-            <div className="name"><b>Name:</b>  {review.name}</div>
-            <div className="user-food-rating"><b>Food-rating:</b>  {review.foodRating}</div>
-            <div className="feedback"><b>Feedback: </b>{review.feedback}</div>
-            <div className="dine-in">{review.dineIn}</div>
-          </div>
-          
+          {reviews.map((review) => (
+            <div className="inner-container">
+              <div className="name"><b>Name:</b>  {review.name}</div>
+              <div className="user-food-rating"><b>Food-rating:</b>  {review.foodRating}</div>
+              <div className="feedback"><b>Feedback: </b>{review.feedback}</div>
+              <div className="dine-in">{review.dineIn}</div>
+            </div>
+
           ))}
         </div>
 
-          
-
-
-
-
-      </>
-    )
+      </div>
+    </>
+  )
 }
