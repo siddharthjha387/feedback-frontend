4bf72ff9d47618ce901774318163e866a6b01169
siddharthjha387 <siddharthjha3901@gmail.com>
2022-12-24 14:27:51 +0530

    backend url to render

--- src/Api_Url.js
+++ src/Api_Url.js
@@ -1,3 +1,2 @@
-// export const API_URL = "https://backend-restaurant-feedback-production.up.railway.app";
-// export const API_URL = "http://localhost:8800";
-export const API_URL = "https://quickfeed-backend.onrender.com";
+
+export const API_URL = "https://quickfeed-backend.onrender.com/";
--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -7,15 +7,15 @@ export default function Home({ myStorage, currentUser, setCurrentUser }) {
 
     return (
         <>
- <div class="top-banner">
+            <div class="top-banner">
                 <div class="container">
-                
+
                     <div class="small-bold-text banner-text">
-                        {currentUser==null?(<> New to QuickFeed : Sign up</>):(<>
-                        If you have any query let us know at &nbsp; 
-                        <a href="mailto:akshit@gmail.com" id='email'>akshit@gmail.com</a>
+                        {currentUser == null ? (<> New to QuickFeed : Sign up</>) : (<>
+                            If you have any query let us know at &nbsp;
+                            <a href="mailto:akshit@gmail.com" id='email'>akshit@gmail.com</a>
                         </>)}
-                       
+
                     </div>
 
                 </div>
@@ -28,7 +28,7 @@ export default function Home({ myStorage, currentUser, setCurrentUser }) {
                     <div class="header-left">
                         <h2>Sell Food Confidently</h2>
                         <p>
-                            Ha bhai reviews dekho apne hotel ke!!
+                            Quick Feed is a platform that takes the feedback of customers and provides it to restaurant owners.
                         </p>
                         <a href="/login" class="primary-button get-started-btn">Get Started</a>
                     </div>
